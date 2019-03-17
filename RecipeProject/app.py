260b8cf57439e42041a4bcd824a4972ff5896206
Jason <3747jason@gmail.com>
2019-03-16 23:19:15 -0700

    added html files and included FlaskForms

--- RecipeProject/app.py
+++ RecipeProject/app.py
@@ -1,20 +1,63 @@
-from flask import Flask, jsonify, request, render_template, url_for
+from flask import Flask, jsonify, request, render_template, url_for, session, redirect
 from flask_restful import Resource, Api
 from flask_login import LoginManager, UserMixin, login_required
 from flask_jwt import JWT
-from security import authenticate, identity
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField,BooleanField,DateTimeField,TextField
+from wtforms.validators import DataRequired
+
 app = Flask(__name__)
 app.secret_key = 'jason'
 login = LoginManager(app)
-jwt = JWT(app, authenticate, identity) # /auth
-
+app.config['SECRET_KEY']== 'jason'
 recipes = [{'name': 'lasagna', 'date added': '3/16/2019','ingredients': ['cheese', 'tomato sauce', 'ground beef'], 
 'serving size': '2 people'},{'name': 'pizza'}]
 
+class DeleteForm(FlaskForm):
+    name = StringField("What recipe would you like to delete?")
+    submit = SubmitField('Submit')
 
-
-
-
+class InfoForm(FlaskForm):
+    name = StringField("What is the name of the recipe?")
+    ingredients = StringField("What are the ingredients involved?")
+    instructions = StringField("What are the instructions to make this recipe?")
+    servingSize = StringField("What is the serving size?")
+    category = StringField("What type of food is this?")
+    notes = StringField("Enter any notes here")
+    dateAdded = StringField("Date added")
+    dateModified = StringField("Date modified")
+    submit = SubmitField('Submit')
+@app.route('/delete-recipe', methods=['GET','POST'])
+def delRecipe():
+    form = DeleteForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('delete.html', form = form)
+    
+## Creates the form to add a new recipe
+@app.route('/add-recipe', methods=['GET', 'POST'])
+def update():
+    
+    form = InfoForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        ingredients = form.ingredients.data
+        form.ingredients.data = ''
+        instructions = form.instructions.data
+        form.instructions.data = ''
+        servingSize = form.servingSize.data
+        form.servingSize.data = ''
+        category = form.category.data
+        form.category.data = ''
+        notes = form.notes.data
+        form.notes.data = ''
+        dateAdded = form.dateAdded.data
+        form.dateAdded.data = ''
+        dateModified = form.dateAdded.data
+        form.dateAdded.data = ''
+    return render_template('add.html', form = form)
 @app.route('/', methods=['GET'])
 def test():
     return render_template('layout.html')
@@ -27,6 +70,10 @@ def returnAllRecipes():
 @app.route('/add-recipe')
 def addRecipeHTML():
     return render_template('add.html')
+
+@app.route('/delete-recipe')
+def deleteRecipeHTML():
+    return render_template('delete.html')
 
 ## Dynamic routing example
 @app.route('/recipes/<name>')
@@ -61,7 +108,11 @@ def editRecipe(recipe):
 def removeRecipe(recipe):
     rec = [recip for recip in recipes if recip['name']==recipe]
     recipes.remove(rec[0])
-    return jsonify({'recipe': recipes})
+    return render_template('delete.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True, port=5003)
