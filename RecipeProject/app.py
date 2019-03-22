021f8819946994b664b2644585bd133cee65aafe
Jason <3747jason@gmail.com>
2019-03-21 23:24:45 -0700

    fixed bug

--- RecipeProject/app.py
+++ RecipeProject/app.py
@@ -10,7 +10,6 @@ from wtforms.validators import DataRequired
 app = Flask(__name__)
 api = Api(app)
 app.secret_key = 'jason'
-login = LoginManager(app)
 app.config['SECRET_KEY']== 'jason'
 recipes = []
 fields = ['Name', 'Ingredients', 'Instructions', 'Serving Size', 'Category', 'Notes', 'Date Added', 'Date Modified']
@@ -41,56 +40,30 @@ class InfoForm(FlaskForm):
     dateModified = StringField("Date modified")
     submit = SubmitField('Submit')
     recipe = []
-
-@app.route('/', methods=['GET'])
-def test():
-    return render_template('layout.html')
     
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
+@app.route('/delete-recipe', methods=['GET','POST'])
+def delRecipe():
+    form = DeleteForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        for i in recipes:
+            if form.name.data == i[0]:
+                recipes.remove(i)
+        return redirect(url_for('delThanks'))        
+    return render_template('delete.html', form = form)
+
+@app.route('/del-recipe/thanks')
+def delThanks():
+    return render_template('deleteThanks.html', recipes = recipes)
+
+@app.route('/delete-recipe')
+def deleteRecipeHTML():
+    return render_template('delete.html')
 
 @app.route('/list-recipes', methods = ['GET'])
 def listRecipesHTML():
     form = InfoForm
     return render_template('listRecipes.html', recipes = recipes, fields = fields)
-
-@app.route('/add-recipe')
-def addRecipeHTML():
-    return render_template('add.html')
-
-@app.route('/add-recipe', methods=['GET', 'POST'])
-def update():
-    recipe = []
-    form = InfoForm()
-    if form.validate_on_submit():
-        session['name'] = form.name.data
-        session['ingredients'] = form.ingredients.data
-        session['instructions'] = form.instructions.data
-        session['servingSize'] = form.servingSize.data
-        session['category'] = form.category.data
-        session['notes'] = form.notes.data
-        session['dateAdded'] = form.dateAdded.data
-        session['dateModified'] = form.dateModified.data
-        recipe.append(form.name.data)
-        recipe.append(form.ingredients.data)
-        recipe.append(form.instructions.data)
-        recipe.append(form.servingSize.data)
-        recipe.append(form.category.data)
-        recipe.append(form.notes.data)
-        recipe.append(form.dateAdded.data)
-        recipe.append(form.dateModified.data)
-        recipes.append(recipe)
-        return redirect(url_for('addThanks'))
-    return render_template('add.html', form = form)
-
-@app.route('/add-recipe/thanks')
-def addThanks():
-    return render_template('addThanks.html')
-
-@app.route('/edit-recipe')
-def editRecipeHTML():
-    return render_template('edit.html', recipes = recipes)
 
 @app.route('/edit-recipe', methods = ['GET', 'POST'])
 def editRecipe():
@@ -125,24 +98,51 @@ def editRecipe():
 def editThanks():
     return render_template('editThanks.html', recipes = recipes)
 
-@app.route('/delete-recipe')
-def deleteRecipeHTML():
-    return render_template('delete.html')
+@app.route('/edit-recipe')
+def editRecipeHTML():
+    return render_template('edit.html', recipes = recipes)
 
-@app.route('/delete-recipe', methods=['GET','POST'])
-def delRecipe():
-    form = DeleteForm()
+## Creates the form to add a new recipe
+@app.route('/add-recipe', methods=['GET', 'POST'])
+def update():
+    recipe = []
+    form = InfoForm()
     if form.validate_on_submit():
         session['name'] = form.name.data
-        for i in recipes:
-            if form.name.data == i[0]:
-                recipes.remove(i)
-        return redirect(url_for('delThanks'))        
-    return render_template('delete.html', form = form)
+        session['ingredients'] = form.ingredients.data
+        session['instructions'] = form.instructions.data
+        session['servingSize'] = form.servingSize.data
+        session['category'] = form.category.data
+        session['notes'] = form.notes.data
+        session['dateAdded'] = form.dateAdded.data
+        session['dateModified'] = form.dateModified.data
+        recipe.append(form.name.data)
+        recipe.append(form.ingredients.data)
+        recipe.append(form.instructions.data)
+        recipe.append(form.servingSize.data)
+        recipe.append(form.category.data)
+        recipe.append(form.notes.data)
+        recipe.append(form.dateAdded.data)
+        recipe.append(form.dateModified.data)
+        recipes.append(recipe)
+        return redirect(url_for('addThanks'))
+    return render_template('add.html', form = form)
 
-@app.route('/del-recipe/thanks')
-def delThanks():
-    return render_template('deleteThanks.html', recipes = recipes)
+@app.route('/add-recipe')
+def addRecipeHTML():
+    return render_template('add.html')
+
+@app.route('/add-recipe/thanks')
+def addThanks():
+    return render_template('addThanks.html')
+
+@app.route('/', methods=['GET'])
+def test():
+    return render_template('layout.html')
+    
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True, port=5003)
